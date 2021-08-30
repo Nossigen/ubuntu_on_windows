44c0b307843128aca5762be875d92e31301c3db8
Nossigen <39987530+Nossigen@users.noreply.github.com>
2021-08-31 00:09:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ sudo apt upgrade -y
 
 sudo apt remove apache2
 sudo apt install -y python3 python3-pip
-sudo apt install -y php
 sudo apt install -y tmux 
+sudo apt install -y fish
+sudo apt install -y cargo
 
+cargo install lsd
 pip3 install tldr
